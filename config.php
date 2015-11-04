a0abf802daad2c8c19fb4eb75b31b918610e8973
Mayank Bansal <sunny.bansal@gmail.com>
2015-11-05 02:55:22 +0530

    IECSE Connect Server
    This is the backend server running using the Google Cloud platform for
    the IECSE connect app.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+ ERROR_REPORTING(0);
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "iecse_connect");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyB7eGY-HGJYYk9B6v4XdDtNlxFLGxqBp9w"); // Place your Google API Key
+?>
